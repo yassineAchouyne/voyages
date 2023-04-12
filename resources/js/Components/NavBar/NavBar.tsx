29cbc0c324cda9a9cd665bf55e04a6cdb7f87fc7
yassineAchouyne <ACHOUYNE.YASSINE@ofppt-edu.ma>
2023-04-12 13:09:32 +0100

    add

---
+++ resources/js/Components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React ,{useState,useEffect}from "react";
+import styles from './NavBar.module.css'
+// import { Inertia } from "@inertiajs/inertia";
+export default function NavBar(props) {
+  
+  if(props.bg) var st = {backgroundColor:"#4A739A"}
+  if(props.scrol)var st = {backgroundColor:"#4A739A"}
+  return (
+    <header  className={styles.menu} style={st}>
+      <img src="./img/pngwing.com.png"/>
+      <nav>
+        <a href="/login" className={styles.connecter}>SE CONNECTER</a> 
+        <a>
+          <img src="./img/pngwing.com (1).jpg"/>
+        </a>
+      </nav>
+    </header>
+  );
+}
